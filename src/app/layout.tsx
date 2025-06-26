2788b3ed536c3a5a7d5b43c48df66af0f7566373
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2025-06-27 07:08:02 +0900

    SEO用のメタタグ登録

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,27 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&display=swap" rel="stylesheet" />
+        {/* SEO & OGP meta tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="IVSサイドイベントをGoogleカレンダーにカンタン登録！" />
+        <meta name="author" content="@yokoe24" />
+        <meta name="theme-color" content="#192638" />
+        <link rel="icon" href="/images/square_logo_alpha.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/images/square_logo_white.png" />
+        <link rel="canonical" href="https://ivs2025.l-master.net" />
+        {/* OGP */}
+        <meta property="og:title" content="IVSサイドイベント to Googleカレンダー" />
+        <meta property="og:description" content="IVSサイドイベントをGoogleカレンダーにカンタン登録！" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ivs2025.l-master.net" />
+        <meta property="og:image" content="/images/square_logo_white.png" />
+        <meta property="og:site_name" content="IVSサイドイベント to Googleカレンダー" />
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="IVSサイドイベント to Googleカレンダー" />
+        <meta name="twitter:description" content="IVSサイドイベントをGoogleカレンダーにカンタン登録！" />
+        <meta name="twitter:image" content="/images/square_logo_white.png" />
       </head>
       <body style={{ position: 'relative' }}>
         {/* 星アニメーション背景 */}
