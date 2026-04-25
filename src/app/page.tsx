573e6847d3951d49a9e0d6518e759e7886454314
ハトネコエ <hato.nekoe@gmail.com>
2026-04-26 01:34:29 +0900

    X にルビを振る

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
           margin: '2em auto',
         }}
       >
-        製作者のXアカウント
+        製作者の<ruby>X<rt>エックス</rt></ruby>アカウント
       </a>
     </main>
   );
